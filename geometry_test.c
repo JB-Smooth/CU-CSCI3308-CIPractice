ed1bc2b1dd552e550058978b2a3761e21446ba99
Conor Walsh <conor.walsh@comcast.net>
2015-04-15 19:15:46 -0600

    Edited geometry.c, geometry.h and geometry_test.c

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,47 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+/* coord_2d_eq Test */
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    a.x = b.x = c.x = 0;
+    a.y = b.y = c.y = 0;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+
+    a.x = b.x = c.x = 9.99;
+    a.y = b.y = c.y = 9.99;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+
+    a.x = b.x = c.x = 3.33;
+    a.y = b.y = c.y = 9.99;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+
+
+    a.x = 3.33;
+    a.y = 9.99;
+    b.x = 2.33;
+    b.y = 9.99;
+    c.x = 5.35;
+    c.y = 9.99;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0 );
+
+    a.x = 1.11;
+    a.y = 2.22;
+    b.x = 7.77;
+    b.y = 8.88;
+    c.x = 3.33;
+    c.y = 4.44;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+
+}
+END_TEST
+
+
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +203,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
